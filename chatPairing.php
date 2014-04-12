6795ead00f86a7de920daf4f4c9e0a539f775077
abbott221 <abbottm07@gmail.com>
2014-04-12 09:56:21 -0400

    chatPairing talks

--- chatPairing.php
+++ chatPairing.php
@@ -1,4 +1,3 @@
-
 <?php
   include 'globals.php';
 ?>
@@ -17,7 +16,15 @@
 
   $uID = $_COOKIE['uid'];
   $condition = array();
+  
+  //a single dimensional array that contains uID and username from the array_push
   $sharingUsers = array();
+  
+  
+  //@Debugging
+  echo "cookie: " . $_COOKIE['uid'] . "\n";
+  echo "stephen's uID var: " . $_COOKIE['uid'] . "\n";
+  
 
   // Get uID and username of non-anon patients
   $query = "SELECT * FROM loginData";
@@ -27,7 +34,12 @@
   {
 	 if ($row['anon'] == 0)
 	 {
+		//pushes on 2 elements: uID and username (heterogeneous array???)
 		array_push($sharingUsers, $row['uID'], $row['username']);
+		//how about just user name OR uID and determine the other value from what you kept later?
+		
+		//@Debugging
+		echo "anon user: " . $row['username'] . "\n";
 	 }
   }
 
@@ -38,7 +50,12 @@
   {
 	 if ($row['uID'] == $_COOKIE['uid'])
 	 {
+		//why also push an empty string?
+		//shouldn't it be: array_push($condition, $row['name']);
 		array_push($condition, $row['name'], "");
+		
+		//@Debugging
+		echo "condition: " . $row['name'] . "\n";
 	 }
   }
 
@@ -49,11 +66,20 @@
   $result = mysqli_query($connect, $query);
   while ($row = mysqli_fetch_array($result))
   {
+	 //@Debugging
+	 echo "Checking: " . $row['uID'] . " " . $row['name'];
+	 
 	 if (array_key_exists($row['uID'], $sharingUsers)
 	 {
+		//@Debugging
+		echo "Check 1 passed";
+		
 		if (array_key_exists($row['name'], $condition)
 		{
 		  $partnerID = $row['uID'];
+		  
+		  //@Debugging
+		  echo "Check 2 passed";
 		}
 	 }
   }
@@ -67,21 +93,40 @@
   $result = mysqli_query($connect, $query);
   while ($row = mysqli_fetch_array($result))
   {
+	 //@Debugging
+	 echo "2 Checking: " . $row['uID'] . " " . $row['partnerID'];
+	 
 	 if ($row['uID'] == $_COOKIE['uid'])
 	 {
+		//@Debugging
+		echo "Check 1 passed";
+		
 		if ($row['partnerID'] == $partnerID)
 		{
+		  //@Debugging
+		  echo "Check 2 passed";
+		  
 		  $partnerID = $row['uID'];
 		}
 	 }
 	 $threadCount = $threadCount + 1;
   }
-  if ($threadID == 0)
-  {
+  
+  //YOU WERE JUST INCREMENTING THREADID, WHY WOULD IT BE ZERO? no, that was thread count
+  //IF THIS IS RUN MULTIPLE TIME, IS IT RESET TO ZERO AT START OF PROGRAM?
+  //why even check if threadID is zero? Why not just set it?
+  //possibly, in case it is already set X it is reset to 0 anyway right before the while loop
+  //if ($threadID == 0)
+  //{
 	 $threadID = $threadCount;
-  }
+  //}
 
   setcookie("threadID", $threadID, time()+3600);
-  header('Location: https://cin.kc8khl.net/cardinalCodefest2014/postsFront.html');
+  
+  
+  //@Debugging
+  //header('Location: https://cin.kc8khl.net/cardinalCodefest2014/postsFront.html');
+  
+  
   mysqli_close($connect);
 ?>
