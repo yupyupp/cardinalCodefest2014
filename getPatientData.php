880503bffb4f751e9f654739aaf66376c5e9bf56
Stephen Haffner <sjhaffner@rohan.kc8khl.net>
2014-04-12 13:21:45 -0400

    Cookies

--- getPatientData.php
+++ getPatientData.php
@@ -100,7 +100,7 @@
   $allList .= $htmlReturn;
 
   setcookie("all", $allList, time()+3600);
-  echo $_COOKIE['all'];
-  //header('Location: https://cin.kc8khl.net/cardinalCodefest2014/requestPage.html');
+
+  header('Location: https://cin.kc8khl.net/cardinalCodefest2014/requestPage.html');
   mysqli_close($connect);
 ?>
