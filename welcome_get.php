d83e264e8e014b0cff77f10a023f3d596ed6cdef
abbott221 <abbottm07@gmail.com>
2014-04-12 00:38:06 -0400

    working on welcome_get.php

---
+++ welcome_get.php
@@ -0,0 +1,40 @@
+//stuff lol comments
+<?php
+	//create connection and store the data for later use
+	//host, username, password, dbname
+	$host = 'http://cin.kc8khl.net';
+	$user = 'michael';
+	$pass = 'passwordz';
+	
+	$db = 'cardinalCodeFest';
+	
+	$connect = mysqli_connect($host,$user,$pass, $db);
+	
+	// Check connection
+	if (mysqli_connect_errno($connect))
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	
+	
+	$query = "SELECT * FROM loginData";
+	$result = mysqli_query($connect, $query);
+	
+	while ($row = mysqli_fetch_array($result))
+	{
+		if ($row['username'] == $_POST['username'])
+		{
+			if ($row['password'] == $_POST['password'])
+			{
+				setcookie("uid", $row['uID'], time()+3600);
+				echo "Account successfully logged in";
+			}
+			else
+				echo "Incorrect password";
+		}
+	}
+	
+	
+	mysqli_close($connect);
+?>
+
